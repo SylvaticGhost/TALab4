bc8c444f61db2e315a218ec08f3fa493e12eb2d8
layron <53446309+layron11@users.noreply.github.com>
2024-03-25 23:56:14 +0200

    Update StudentBlock.tsx

--- app/сomponents/StudentBlock.tsx
+++ app/сomponents/StudentBlock.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
 
 
+
 type StudentProps = {
     name: string,
     grade: number,
-    image: HTMLImageElement
 }
 export default function StudentBlock (props:StudentProps) {
 
@@ -12,7 +12,7 @@ export default function StudentBlock (props:StudentProps) {
         <tr className="bg-white border-b ">
             <th scope="row"
                 className="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                <Image src={props.image} className="w-10 mr-2 h-10 rounded" alt="Мое изображение" width={500} height={300} />
+                {/*<Image src={props.image} className="w-10 mr-2 h-10 rounded" alt="Мое изображение" width={500} height={300} />*/}
 
                 {props.name}
             </th>
