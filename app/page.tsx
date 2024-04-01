d82224ca61ddc344a0c3824d7d9684d4a141569a
layron <53446309+layron11@users.noreply.github.com>
2024-04-01 17:43:22 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,35 @@
 'use client'
-import Image from "./img/kit.jpg";
-import Image2 from "./img/kit2.jpeg";
-import Image3 from "./img/kit3.jpg";
-import Image4 from "./img/kit4.jpg";
-
-import {Queue} from "@/app/classes/Queue";
+import Chart from "@/app/components/Chart";
+import './interfaces/main'
+import {Stack} from "@/app/classes/Stack";
 
 
 import {useState} from "react";
-import StudentBlock from './components/StudentBlock'
 import StudentBase from "@/app/components/StudentBase";
-import {number} from "prop-types";
 
 interface Yearbook {
+    group: string,
     person: string;
     grade: number;
 }
+
+interface ChartProps {
+    data: DataPoint[] | undefined
+}
+
+interface DataPoint {
+    year: string
+    average: number
+}
+
 export default function Home() {
     const grades = [
-        { grade: 'A', description: 'Excellent' },
-        { grade: 'B', description: 'Very good' },
-        { grade: 'C', description: 'Good' },
-        { grade: 'D', description: 'Satisfactory' },
-        { grade: 'E', description: 'Sufficient' },
-        { grade: 'F', description: 'Unsatisfactory' }
+        {grade: 'A', description: 'Excellent'},
+        {grade: 'B', description: 'Very good'},
+        {grade: 'C', description: 'Good'},
+        {grade: 'D', description: 'Satisfactory'},
+        {grade: 'E', description: 'Sufficient'},
+        {grade: 'F', description: 'Unsatisfactory'}
     ]
     //const images = [Image, Image2, Image3, Image4]
 
@@ -47,28 +53,30 @@ export default function Home() {
         if (mark < 0) {
             setMark(0)
             return false
-        } else if (mark>100) {
+        } else if (mark > 100) {
             setMark(100)
             return false
         }
         return true
     }
 
-    let validateInputMark = (mark:any) => {
+    let validateInputMark = (mark: any) => {
         return !isNaN(mark);
 
     }
     let [mark, setMark] = useState<number>(0);
+    let [group, setGroup] = useState('IC-3')
     let [name, setName] = useState<string>('')
     let [year, setYear] = useState<string>('2024')
-    let [stats, setStats] = useState([])
+    let [stats, setStats] = useState<ChartProps|undefined>()
+    let [hiddenStats, setHiddenStats] = useState<string|undefined>('hidden')
 
-    const [grades2024, setGrades2024] = useState(new Queue<Yearbook>());
-    const [grades2023, setGrades2023] = useState(new Queue<Yearbook>());
-    const [grades2022, setGrades2022] = useState(new Queue<Yearbook>());
-    const [grades2021, setGrades2021] = useState(new Queue<Yearbook>());
-    const [grades2020, setGrades2020] = useState(new Queue<Yearbook>());
-    const [grades2019, setGrades2019] = useState(new Queue<Yearbook>());
+    const [grades2024, setGrades2024] = useState(new Stack<Yearbook>());
+    const [grades2023, setGrades2023] = useState(new Stack<Yearbook>());
+    const [grades2022, setGrades2022] = useState(new Stack<Yearbook>());
+    const [grades2021, setGrades2021] = useState(new Stack<Yearbook>());
+    const [grades2020, setGrades2020] = useState(new Stack<Yearbook>());
+    const [grades2019, setGrades2019] = useState(new Stack<Yearbook>());
 
     const gradeSetters = {
         '2024': setGrades2024,
@@ -78,33 +86,39 @@ export default function Home() {
         '2020': setGrades2020,
         '2019': setGrades2019
     };
+
     function addToBase(name: string, mark: number) {
-        if (name.trim() != '' && !isNaN(mark)) {
+        if (group == 'IC-' || group.length != 5) {
+            alert('Incorrect group input')
+        } else if (name.trim() == '') {
+            alert('Incorrect name input')
+        } else if (isNaN(mark)) {
+            alert('Incorrect grade input')
+        } else {
             // @ts-ignore
             const setGrades = gradeSetters[year];
 
             if (setGrades) {
                 setGrades((oldGrades: { toArray: () => any[]; }) => {
                     if (oldGrades.toArray().some(item => item.person === name)) {
-                        alert(`Студент з ім'ям ${name} вже існує в черзі.`);
+                        alert(`THe student with name ${name} is already in the stack`);
                         return oldGrades;
                     }
 
-                    let newGrades = new Queue<Yearbook>();
-                    oldGrades.toArray().forEach(item => newGrades.enqueue(item));
-                    newGrades.enqueue({ person: name, grade: mark });
+                    let newGrades = new Stack<Yearbook>();
+                    oldGrades.toArray().forEach(item => newGrades.push(item));
+                    newGrades.push({group: group, person: name, grade: mark});
                     return newGrades;
                 });
             } else {
-                console.error(`Невідомий рік: ${year}`);
+                console.error(`Invalid year: ${year}`);
             }
-        } else {
-            alert('Не введені дані')
         }
 
     }
 
     function calculateAverage() {
+        setHiddenStats('')
         const gradeQueues = {
             '2024': grades2024,
             '2023': grades2023,
@@ -113,6 +127,7 @@ export default function Home() {
             '2020': grades2020,
             '2019': grades2019
         };
+        let chartStats = []
         let newStats = []
         for (const c_year in gradeQueues) {
             // @ts-ignore
@@ -120,12 +135,19 @@ export default function Home() {
             if (grades.length > 0) {
                 const sum = grades.reduce((total: any, grade: { grade: any; }) => total + grade.grade, 0);
                 const average = sum / grades.length;
-                newStats.push(`Середній бал для ${c_year}: ${average.toFixed(2)}`)
+
+                newStats.push({
+                    year: c_year,
+                    average: average.toFixed(2)
+                })
             } else {
-                newStats.push(`Немає даних для ${c_year}`);
+                newStats.push({
+                    year: c_year,
+                    average: undefined
+                })
             }
         }
-
+        console.log(stats)
         // @ts-ignore
         setStats(newStats)
     }
@@ -137,16 +159,34 @@ export default function Home() {
         const setGrades = gradeSetters[year];
 
         if (setGrades) {
-            setGrades(new Queue<Yearbook>());
+            setGrades(new Stack<Yearbook>());
             setStats([])
         } else {
             console.error(`Невідомий рік: ${year}`);
         }
     }
 
-    function compareGrades(queue1: Queue<Yearbook>, queue2: Queue<Yearbook>, queue3: Queue<Yearbook>, queue4: Queue<Yearbook>, queue5: Queue<Yearbook>, queue6: Queue<Yearbook>) {
+    function compareGrades(queue1: Stack<Yearbook>, queue2: Stack<Yearbook>, queue3: Stack<Yearbook>, queue4: Stack<Yearbook>, queue5: Stack<Yearbook>, queue6: Stack<Yearbook>) {
 
     }
+
+    function checkGroupHandler(e: string) {
+            return !(!e.startsWith("IC-3") || e.length > 5 || e.includes(' ') || e[4] == '0');
+
+    }
+
+    function findTopYears(data: ChartProps | { average: number; }[] | undefined) {
+        if (!data) return [];
+
+        const filteredData = data?.filter((item: { average: undefined; }) => item && item.average !== undefined);
+
+        // Сортируем массив по убыванию значения average
+        const sortedData = filteredData.sort((a: { average: number; }, b: { average: number; }) => b.average - a.average);
+
+        // Берем первые три элемента (года с наибольшим average)
+        return sortedData.slice(0, 3).map((item: { year: any; }) => item.year);
+    }
+
 
     return (
         <>
@@ -180,6 +220,25 @@ export default function Home() {
             })()}
 
             <div className="flex mx-auto w-11/12">
+                <div className="max-w-lg mx-auto mr-0">
+                    <label htmlFor="group-input"
+                           className="block mb-2 text-sm font-medium text-gray-900">Group name:</label>
+                    <div className="flex">
+    <span
+        className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-300 rounded-s-md ">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" width={22} height={22}>
+          <path fill="#6b7280"
+                d="M72 88a56 56 0 1 1 112 0A56 56 0 1 1 72 88zM64 245.7C54 256.9 48 271.8 48 288s6 31.1 16 42.3V245.7zm144.4-49.3C178.7 222.7 160 261.2 160 304c0 34.3 12 65.8 32 90.5V416c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V389.2C26.2 371.2 0 332.7 0 288c0-61.9 50.1-112 112-112h32c24 0 46.2 7.5 64.4 20.3zM448 416V394.5c20-24.7 32-56.2 32-90.5c0-42.8-18.7-81.3-48.4-107.7C449.8 183.5 472 176 496 176h32c61.9 0 112 50.1 112 112c0 44.7-26.2 83.2-64 101.2V416c0 17.7-14.3 32-32 32H480c-17.7 0-32-14.3-32-32zm8-328a56 56 0 1 1 112 0A56 56 0 1 1 456 88zM576 245.7v84.7c10-11.3 16-26.1 16-42.3s-6-31.1-16-42.3zM320 32a64 64 0 1 1 0 128 64 64 0 1 1 0-128zM240 304c0 16.2 6 31 16 42.3V261.7c-10 11.3-16 26.1-16 42.3zm144-42.3v84.7c10-11.3 16-26.1 16-42.3s-6-31.1-16-42.3zM448 304c0 44.7-26.2 83.2-64 101.2V448c0 17.7-14.3 32-32 32H288c-17.7 0-32-14.3-32-32V405.2c-37.8-18-64-56.5-64-101.2c0-61.9 50.1-112 112-112h32c61.9 0 112 50.1 112 112z"/></svg>
+    </span>
+                        <input type="text" id="group-input"
+                               className="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-24 text-sm p-2.5  "
+                               placeholder="Harley Queen" value={group} onInput={(e) => {
+                            e.target.value = e.target.value.replace(/[^a-zA-Zа-яёА-ЯЁіІєЄїЇґҐ0-4-\s]/g, '') //TODO: fix
+                        }} onChange={(e) => {
+                            if (checkGroupHandler(e.target.value)) {setGroup(e.target.value)}
+                        }}/>
+                    </div>
+                </div>
                 <div className="max-w-lg mx-auto">
                     <label htmlFor="website-admin"
                            className="block mb-2 text-sm font-medium text-gray-900">Student`s name:</label>
@@ -303,13 +362,26 @@ export default function Home() {
             </div>
             <div className="flex justify-center mt-10">
                 <button type="button" onClick={calculateAverage}
-                        className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Calculate statistics
+                        className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Calculate
+                    statistics
                 </button>
             </div>
-            <div className="ml-52">
-                {stats.map(stat =>
-                    <p> {stat}</p>
-                )}
+            <div className={hiddenStats + " ml-52 flex items-end mb-6"}>
+                <div className="mr-2">
+                    <p className="pl-6">{findTopYears(stats)[1]}</p>
+                    <div className="h-60 w-20 bg-yellow-200"></div>
+                </div>
+                <div className="mr-2">
+                    <p className="pl-6">{findTopYears(stats)[0]}</p>
+                    <div className="h-80 w-20 bg-green-200"></div>
+                </div>
+                <div className="mr-2">
+                    <p className="pl-6">{findTopYears(stats)[2]}</p>
+                    <div className="h-40 w-20 bg-red-200"></div>
+                </div>
+                <div className="ml-24">
+                    <Chart data={stats}></Chart>
+                </div>
             </div>
         </>
     );
