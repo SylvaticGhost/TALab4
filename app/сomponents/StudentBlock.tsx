ea7e8489055e6288a8e2638de46661f56037df47
layron <53446309+layron11@users.noreply.github.com>
2024-04-01 17:41:45 +0300

    Update StudentBlock.tsx

--- app/сomponents/StudentBlock.tsx
+++ app/сomponents/StudentBlock.tsx
@@ -1,26 +1,32 @@
-import Image from 'next/image'
-
-
-
+interface Yearbook {
+    person: string;
+    grade: number;
+}
 type StudentProps = {
+    id: number,
     name: string,
     grade: number,
 }
 export default function StudentBlock (props:StudentProps) {
-
+    let createStack = (n: number) => {
+        const descendingNumbers = Array.from({ length: n }, (_, i) => (n - i).toString());
+        return descendingNumbers.join(", ");
+    }
     return (
         <tr className="bg-white border-b ">
+            <td className="px-6 py-3">
+                #{props.id}
+            </td>
             <th scope="row"
-                className="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                {/*<Image src={props.image} className="w-10 mr-2 h-10 rounded" alt="Мое изображение" width={500} height={300} />*/}
-
+                className="flex items-center px-6 py-4 pt-6 font-medium text-gray-900 whitespace-nowrap">
                 {props.name}
             </th>
             <td className="px-6 py-4">
                 {props.grade}/100
             </td>
-            <td className="">
-
+            <td className="px-6 py-4 text-xs">
+                Додано роботу #{props.id} <br/>
+                Стопка робіт - {createStack(props.id)}
             </td>
         </tr>
     )
