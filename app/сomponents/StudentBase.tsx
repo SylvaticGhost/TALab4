6d0b95f1048a7b255fc3a804426545a4dde2c4e0
layron <53446309+layron11@users.noreply.github.com>
2024-04-01 17:41:04 +0300

    Update StudentBase.tsx

--- app/сomponents/StudentBase.tsx
+++ app/сomponents/StudentBase.tsx
@@ -1,36 +1,39 @@
-import {Queue} from "@/app/classes/Queue";
+import {Stack} from "@/app/classes/Stack";
 import {number} from "prop-types";
-import StudentBlock from "./StudentBlock";
-
+import StudentBlock from "@/app/components/StudentBlock";
 
 interface Yearbook {
     person: string;
     grade: number;
 }
 type StudentBaseProps = {
-    base: Queue<Yearbook>;
+    base: Stack<Yearbook>;
 }
 // @ts-ignore
 export default function StudentBase({base})  {
-    // @ts-ignore
-    // @ts-ignore
     return (
         <div className="w-9/12 mx-auto mt-10 mb-40 text-center relative overflow-x-auto">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50">
                 <tr>
                     <th scope="col" className="px-6 py-3">
+                        ID
+                    </th>
+                    <th scope="col" className="px-6 py-3">
                         Student`s name
                     </th>
                     <th scope="col" className="px-6 py-3">
                         Grade
                     </th>
+                    <th scope="col" className="px-6 py-3">
+                        Stack
+                    </th>
                 </tr>
                 </thead>
                 <tbody>
-                {base.toArray().map(item =>
+                {base.toArray().map((item: { group:string, person: string; grade: number; }, index: number) =>
                     // eslint-disable-next-line react/jsx-key
-                    <StudentBlock name={item.person} grade={item.grade}></StudentBlock>
+                    <StudentBlock name={item.group + ' ' + item.person} grade={item.grade} id={index+1}></StudentBlock>
                 )}
                 </tbody>
             </table>
